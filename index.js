64484f8777371265ce0be2b35cfdeddbfce27f73
bao ann <baoanh1buinguyen@gmail.com>
2023-07-08 23:59:53 +0700

    updateversion40

--- index.js
+++ index.js
@@ -6,10 +6,25 @@ const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 const $create = document.createElement.bind(document);
 const $createcomment = document.createComment.bind(document);
-const px = $('p');
-const p = $$('p')[1];
+// binding method
+const root = document.querySelector(':root');
+const proproot = getComputedStyle(root);
+const getprop = proproot.getPropertyValue.bind(proproot);
+
+// binding method
+const rootstyle = root.style;
+const setprop = rootstyle.setProperty.bind(rootstyle);
+
+const px = $('#viewer p');
+const p = $$('#viewer p')[1];
+const viewer = $('#viewer');
 let pnode = px.firstChild;
 let timeout = null;
+let ready = 0;
+let unit =
+    parseFloat(getComputedStyle(document.documentElement).fontSize) /
+    (20 / 1.4);
+setprop('--unit', `${unit}px`);
 const list = new (function () {
     this._construct = function (
         letter,
@@ -24,7 +39,8 @@ const list = new (function () {
         ps_up_for_f = []
     ) {
         this[letter] = {
-            width: width / 7,
+            width: font.charToGlyph(letter).advanceWidth / 70,
+            width_: width / 7,
             ps_up, //(o duoi)
             ps_down, //(o tren)
             st_up,
@@ -66,7 +82,13 @@ const list = new (function () {
         );
     };
 
-    this.cap = function (letter, width, ps_up, ps_down, ps_up_for_f = []) {
+    this.cap = function (
+        letter,
+        width,
+        ps_up = [],
+        ps_down = [],
+        ps_up_for_f = []
+    ) {
         this._construct(
             letter,
             width,
@@ -84,8 +106,8 @@ const list = new (function () {
     this.lowercase = function (
         letter,
         width,
-        ps_up,
-        ps_down,
+        ps_up = [],
+        ps_down = [],
         ps_up_for_f = []
     ) {
         this._construct(
@@ -104,83 +126,140 @@ const list = new (function () {
 
     return this;
 })();
-list.as('b', 70, [2], [2], 2, [5]);
-list.as('d', 70, [9], [9], 9, [5]);
-list.as('f', 35, [3], [], 4);
-list.as('h', 63, [2, 8], [2], 2);
-list.as('k', 56, [2, 8], [2], 2);
-list.as('l', 21, [2], [2], 2);
 
-list.des('g', 70, [], [9], 8);
-list.des('j', 21, [], [2], 1);
-list.des('p', 70, [], [2], 2, [5]);
-list.des('q', 70, [], [9], 9, [5]);
-list.des('y', 63, [8], [2, 8], 8, [3]);
-list.des('t', 35, [3], [3], 3);
+buffer.then((data) => {
+    font = opentype.parse(data);
+    /*
+    let glyphA = font.charToGlyph('r');
+    let glyphV = font.charToGlyph('e');
+    console.log();
+    font.getKerningValue(font.charToGlyph('r'), font.charToGlyph('e'))/10
+    */
+    // ascender
+    list.as('b', 70, [2], [], 2, [5]);
+    list.as('d', 70, [9], [], 9, [5]);
+    list.as('f', 35, [3], [], 4);
+    list.as('h', 63, [2, 8], [], 2);
+    list.as('k', 56, [2, 8], [], 2);
+    list.as('l', 21, [2], [], 2);
+    // descender
+    list.des('g', 70, [], [9], 8);
+    list.des('j', 21, [], [2], 1);
+    list.des('p', 70, [], [2], 2, [5]);
+    list.des('q', 70, [], [9], 9, [5]);
+    list.des('y', 63, [], [2, 8], 8, [3]);
+    list.des('t', 35, [], [3], 3);
+    // lowercase
+    list.lowercase('a', 56, [7], [], [3, 4]);
+    list.lowercase('c', 56, [], [], [4, 5]);
+    list.lowercase('ç', 56);
+    list.lowercase('e', 56, [], [], [4]);
+    list.lowercase('é', 56, [], [], [4]);
+    list.lowercase('ê', 56, [], [], [4]);
+    list.lowercase('è', 56, [], [], [4]);
+    list.lowercase('i', 21, [2], [2]);
+    list.lowercase('m', 84, [2, 11], [2]);
+    list.lowercase('n', 63, [2, 8], [2]);
+    list.lowercase('ñ', 63, [2, 8], [2]);
+    list.lowercase('o', 63, [], [], [4, 5]);
+    list.lowercase('r', 42, [2], [2]);
+    list.lowercase('s', 49, [], [], [5]);
+    list.lowercase('u', 63, [8], [2, 8], [3, 4]);
+    list.lowercase('ü', 63, [8], [], [3, 4]);
+    list.lowercase('v', 49, [], [1]);
+    list.lowercase('w', 84, [], [2, 6]);
+    list.lowercase('x', 49, [7], [1]);
+    list.lowercase('z', 49);
+    // punctuation
+    list.lowercase(' ', 49);
+    list.lowercase('.', 28);
+    list.lowercase(',', 21, [2], []);
+    list.lowercase(';', 28, [3], []);
+    list.lowercase(':', 28);
+    list.cap('!', 35, [], [3]);
+    list.cap('?', 49, [], [3, 5]);
+    list.lowercase('#', 77, [2, 5], [], [8]);
+    list.cap('/', 35, [], [5]);
+    list.lowercase('-', 56);
+    list.lowercase('–', 63);
+    list.lowercase('—', 84);
+    list.lowercase('_', 63);
+    list.lowercase('(', 35);
+    list.lowercase(')', 35);
+    list.lowercase(']', 35);
+    list.lowercase('[', 35);
+    list.lowercase('"', 35);
+    list.lowercase('“', 35);
+    list.lowercase('”', 35);
+    list.lowercase("'", 21);
+    list.lowercase('’', 21);
+    list.lowercase('‘', 21);
+    list.lowercase('@', 84);
+    list.lowercase('&', 77, [8], []);
+    list.lowercase('$', 70, [5], [5]);
+    list.lowercase('+', 63);
+    list.lowercase('=', 63);
+    list.lowercase('>', 63);
+    list.lowercase('<', 63);
+    list.lowercase('~', 56);
+    list.lowercase('^', 42);
+    //cap
+    list.cap('A', 63, [9], [5]);
+    list.cap('B', 63, [2]);
+    list.cap('C', 63, [], [], [6, 7]);
+    list.cap('D', 77, [2]);
+    list.cap('E', 49, [2], []);
+    list.cap('F', 49, [2], []);
+    list.cap('G', 84, [11], [], [7, 8]);
+    list.cap('H', 70, [2, 9], [2, 9]);
+    list.cap('I', 21, [2], [2]);
+    list.cap('J', 21, [], [2]);
+    list.cap('K', 63, [2, 9], [2]);
+    list.cap('L', 49, [], [2]);
+    list.cap('M', 91, [2, 7, 11], [2]);
+    list.cap('N', 77, [2, 10], [2, 10]);
+    list.cap('O', 84, [], [], [6, 7]);
+    list.cap('P', 63, [2], []);
+    list.cap('Q', 84, [7], [], [6]);
+    list.cap('R', 63, [2, 9], []);
+    list.cap('S', 63, [], [], [4, 5, 6]);
+    list.cap('T', 63, [5], []);
+    list.cap('U', 77, [], [2, 10], [5, 6]);
+    list.cap('V', 56, [], [1]);
+    list.cap('W', 105, [], [1, 8]);
+    list.cap('X', 63, [9], [1]);
+    list.cap('Y', 63, [5], [1, 9]);
+    list.cap('Z', 70, [], []);
+    // number
+    list.lowercase('0', 56, [], [], [4, 5]);
+    list.lowercase('1', 21, [2]);
+    list.cap('2', 49);
+    list.lowercase('3', 42);
+    list.lowercase('4', 49);
+    list.cap('5', 49);
+    list.cap('6', 56, [], [], [4]);
+    list.lowercase('7', 49);
+    list.cap('8', 63);
+    list.lowercase('9', 63);
 
-list.lowercase('a', 56, [7], [], [3, 4]);
-list.lowercase('c', 56, [], [], [4, 5]);
-list.lowercase('e', 56, [], [], [4]);
-list.lowercase('i', 21, [2], [2]);
-list.lowercase('m', 84, [2, 11], [2, 6, 11]);
-list.lowercase('n', 63, [2, 8], [2, 8]);
-list.lowercase('o', 63, [], [], [4, 5]);
-list.lowercase('r', 42, [2], [2]);
-list.lowercase('s', 49, [], [], [3, 4]);
-list.lowercase('u', 63, [8], [2, 8], [3, 4]);
-list.lowercase('v', 49, [], [1]);
-list.lowercase('w', 84, [2, 3, 7, 8, 11], [2, 11]);
-list.lowercase('x', 49, [1, 7], [1, 7]);
-list.lowercase('z', 42, [2, 3, 4, 5], [2, 3, 4, 5]);
+    for (const i in list) {
+        if (list[i].width) {
+            if (list[i].width != list[i].width_) {
+                console.log(i, list[i].width, list[i].width_);
+            }
+        }
+    }
 
-list.lowercase(' ', 49, [], []);
-list.lowercase('.', 28, [], []);
-list.lowercase(',', 21, [2], []);
-list.lowercase(';', 28, [3], []);
-list.lowercase(':', 28, [], []);
-list.cap('!', 35, [], [3]);
-list.cap('?', 35, [], [2, 4]);
-list.lowercase('#', 77, [2, 5], [], [8]);
-list.cap('/', 35, [], [5]);
-list.lowercase('-', 63, [], []);
-list.lowercase('_', 63, [], []);
-list.lowercase('(', 35, [], []);
-list.lowercase(')', 35, [], []);
-list.lowercase(')', 35, [], []);
-list.lowercase('"', 35, [], []);
-list.lowercase("'", 21, [], []);
-list.lowercase('@', 84, [], []);
-list.lowercase('&', 77, [8], []);
-list.lowercase('$', 70, [5], [5]);
-list.lowercase('+', 63, [], []);
-list.lowercase('=', 63, [], []);
+    readyToExecute();
+});
 
-list.cap('A', 63, [1, 9], [5]);
-list.cap('B', 63, [2], [2]);
-list.cap('C', 63, [], [], [6, 7]);
-list.cap('D', 70, [2], [2]);
-list.cap('E', 49, [2, 3, 4, 5], [2, 3, 4, 5, 6]);
-list.cap('F', 49, [2], [2, 3, 4, 5, 6]);
-list.cap('G', 84, [11], [], [7, 8]);
-list.cap('H', 70, [2, 9], [2, 9]);
-list.cap('I', 21, [2], [2]);
-list.cap('J', 21, [1], [2]);
-list.cap('K', 63, [2, 9], [2]);
-list.cap('L', 49, [2, 3, 4, 5, 6], [2]);
-list.cap('M', 91, [2, 7, 11], [2]);
-list.cap('N', 77, [2, 10], [2, 10]);
-list.cap('O', 84, [6, 7], [6, 7], [6]);
-list.cap('P', 63, [2], [2]);
-list.cap('Q', 84, [7], [6, 7]);
-list.cap('R', 63, [2, 9], [2, 3, 4, 5]);
-list.cap('S', 63, [5], [5], [4, 5]);
-list.cap('T', 49, [4], [2, 3, 4, 5, 6]);
-list.cap('U', 77, [6], [2, 10], [5, 6]);
-list.cap('V', 56, [], [1]);
-list.cap('W', 112, [], [2, 9]);
-list.cap('X', 63, [1, 9], [1, 9]);
-list.cap('Y', 63, [5], [1, 9]);
-list.cap('Z', 70, [5], [5]);
+window.addEventListener('load', readyToExecute);
+
+function readyToExecute() {
+    if (++ready == 2) {
+        extractLinesFromTextNode(pnode);
+    }
+}
 
 function extractLinesFromTextNode(textNode) {
     if (textNode.nodeType !== 3) {
@@ -201,10 +280,15 @@ function extractLinesFromTextNode(textNode) {
     let down_arr = [];
     let down_arr_tong = [];
     let up_arr_span = [];
+    let all_span = [];
 
     let count = 0;
     let tong = 0;
     let spancount = 0;
+
+    let render_arr = [[]];
+
+    let textconsole = [''];
 
     lines.push((lineCharacters = []));
     up_arr_tong.push((up_arr = []));
@@ -214,22 +298,105 @@ function extractLinesFromTextNode(textNode) {
     myEndFunction();
 
     async function myEndFunction() {
+        let baretxt = '';
         for (let i = 0; i < textContent.length; i++) {
             range.setStart(textNode, 0);
             range.setEnd(textNode, i + 1);
             if (count != range.getClientRects().length - 1) {
                 count++;
+                textconsole[count] = '';
                 tong = 0;
                 lines.push((lineCharacters = []));
                 up_arr_tong.push((up_arr = []));
 
                 down_arr_span_tong.push((down_arr_span = []));
                 down_arr_tong.push((down_arr = []));
+
+                render_arr.push([]);
+            }
+            let check = false;
+            let string = textContent[i];
+            let char = list[string];
+            let kern = 0;
+            let kernr = 0;
+            if (tong != 0) {
+                kern =
+                    font.getKerningValue(
+                        font.charToGlyph(textContent[i - 1]),
+                        font.charToGlyph(string)
+                    ) / 70;
+            }
+            if (i + 1 < textContent.length) {
+                kernr =
+                    font.getKerningValue(
+                        font.charToGlyph(string),
+                        font.charToGlyph(textContent[i + 1])
+                    ) / 70;
+            }
+            tong += kern;
+            //
+            textconsole[count] += string;
+            // do not need to check
+            if (count > 0) {
+                if (char.st_up) {
+                    let ua = up_arr_tong[count - 1][tong + char.vl_up - 1]; // + kern
+                    ua = ua == undefined ? { chek: true, for_f: true } : ua;
+                    if (ua.chek) {
+                        if (ua.for_f) {
+                            check = string == 'f';
+                        } else {
+                            check = true;
+                        }
+                    }
+                }
+            } else {
+                if (char.st_up) {
+                    check = true;
+                }
+            }
+            //
+
+            if (char.st_down) {
+                check = true;
             }
 
-            let check = false;
-            let char = list[textContent[i]];
-            //
+            // render_arr
+            if (check) {
+                let span = $create('span');
+                span.append(string);
+                span.wght = 'alt';
+                span.style.marginLeft = `calc(${kern} * var(--unit))`;
+                span.style.marginRight = `calc(${kernr} * var(--unit))`;
+                if (char.st_down) {
+                    let tempvl = tong + char.vl_down - 1;
+                    //let count = spancount;
+                    down_arr_span.push({
+                        tempvl,
+                        count: spancount,
+                        nested1: count,
+                        nested2:
+                            baretxt == ''
+                                ? render_arr[count].length
+                                : render_arr[count].length + 1,
+                    });
+                }
+
+                if (baretxt == '') {
+                    render_arr[count].push(span);
+                    //p_.append(span);
+                } else {
+                    render_arr[count].push(baretxt, span);
+                    //p_.append(baretxt, span);
+                }
+
+                baretxt = '';
+                spancount++;
+            } else {
+                baretxt += string;
+                //p_.append(string);
+            }
+
+            // + kern
             for (let i = 1; i <= char.width; i++) {
                 up_arr.push({
                     chek: false,
@@ -245,52 +412,16 @@ function extractLinesFromTextNode(textNode) {
                 up_arr[tong + char.ps_up_for_f[i] - 1].chek = true;
                 up_arr[tong + char.ps_up_for_f[i] - 1].for_f = true;
             }
-            // do not need to check
+            // + kern
             if (count > 0) {
-                if (char.st_up) {
-                    let ua = up_arr_tong[count - 1][tong + char.vl_up - 1];
-                    ua = ua == undefined ? { chek: true, for_f: true } : ua;
-                    if (ua.chek) {
-                        if (ua.for_f) {
-                            check = textContent[i] == 'f';
-                        } else {
-                            check = true;
-                        }
-                    }
-                }
-
+                // + kern
                 for (let i = 1; i <= char.width; i++) {
                     down_arr.push([false, char.cap]);
                 }
                 for (let i = 0; i < char.ps_down.length; i++) {
                     down_arr[tong + char.ps_down[i] - 1][0] = true;
                 }
-            } else {
-                if (char.st_up) {
-                    check = true;
-                }
-            }
-            //
-
-            if (char.st_down) {
-                check = true;
-            }
-
-            // render
-            if (check) {
-                let span = $create('span');
-                span.append(textContent[i]);
-                p_.append(span);
-                if (char.st_down) {
-                    let tempvl = tong + char.vl_down - 1;
-                    let count = spancount;
-                    down_arr_span.push({ tempvl, count });
-                } else {
-                    up_arr_span.push(spancount);
-                }
-                spancount++;
-            } else {
-                p_.append(textContent[i]);
+                // + kern
             }
 
             tong += char.width;
@@ -299,48 +430,91 @@ function extractLinesFromTextNode(textNode) {
 
         let span_ = $create('span');
         span_.innerHTML = 'a';
-        p_.append($create('br'), span_);
-        p.innerHTML = '';
-        p.append(p_);
+        if (baretxt == '') {
+            render_arr[count].push($create('br'), span_);
+            //p_.append($create('br'), span_);
+        } else {
+            render_arr[count].push(baretxt, $create('br'), span_);
+            //p_.append(baretxt, $create('br'), span_);
+        }
 
-        await wait(100);
         //descender
-        const desspan = p.querySelectorAll('span');
+        //const desspan = p.querySelectorAll('span');
         for (let i = 0; i < down_arr_span_tong.length - 1; i++) {
             let spans = down_arr_span_tong[i];
             let arrs = down_arr_tong[i + 1];
 
             for (let a = 0; a < spans.length; a++) {
                 if (arrs[spans[a].tempvl] == undefined) {
-                    desspan[spans[a].count].classList.add('alt');
-                    continue;
-                }
-                if (arrs[spans[a].tempvl][0]) {
-                    desspan[spans[a].count].classList.add(
+                    render_arr[spans[a].nested1][spans[a].nested2].wght = 'alt';
+                    //desspan[spans[a].count].classList.add('alt');
+                    //continue;
+                } else if (arrs[spans[a].tempvl][0]) {
+                    render_arr[spans[a].nested1][spans[a].nested2].wght = arrs[
+                        spans[a].tempvl
+                    ][1]
+                        ? 'altshort'
+                        : 'alt';
+                    /*desspan[spans[a].count].classList.add(
                         arrs[spans[a].tempvl][1] ? 'altshort' : 'alt'
-                    );
+                    );*/
+                    //continue;
+                } else {
+                    render_arr[spans[a].nested1][spans[a].nested2] =
+                        render_arr[spans[a].nested1][
+                            spans[a].nested2
+                        ].innerHTML;
                 }
             }
         }
         //ascender
+        /*
         for (let i = 0; i < up_arr_span.length; i++) {
             desspan[up_arr_span[i]].classList.add('alt');
         }
+        */
         //lastline
+        /*
         let spans = down_arr_span_tong[down_arr_span_tong.length - 1];
         for (let a = 0; a < spans.length; a++) {
             desspan[spans[a].count].classList.add('alt');
         }
+        */
+
+        // fully render
+        for (let i = 0; i < render_arr.length; i++) {
+            let baretxt = '';
+            for (let k = 0; k < render_arr[i].length; k++) {
+                if (typeof render_arr[i][k] == 'string') {
+                    baretxt += render_arr[i][k];
+                } else {
+                    if (baretxt == '') {
+                        p_.append(render_arr[i][k]);
+                    } else {
+                        p_.append(baretxt, render_arr[i][k]);
+                    }
+                    baretxt = '';
+                }
+            }
+            if (baretxt != '') {
+                p_.append(baretxt);
+            }
+        }
+        p.innerHTML = '';
+        p.append(p_);
+        // animation
+        await wait(100);
+        let allspan = p.querySelectorAll('span');
+        for (let i = 0; i < allspan.length; i++) {
+            allspan[i].classList.add(allspan[i].wght);
+        }
+        console.log(textconsole);
     }
 }
 
 function collapseWhiteSpace(value) {
     return value.trim().replace(/\s+/g, ' ');
 }
-
-window.addEventListener('load', function () {
-    extractLinesFromTextNode(pnode);
-});
 
 function wait(delay) {
     return new Promise((resolve) => setTimeout(resolve, delay));
@@ -359,9 +533,13 @@ async function setText() {
     extractLinesFromTextNode(pnode);
 }
 
-window.onresize = onresize_;
+//window.onresize = onresize_;
+const resizeObserver = new ResizeObserver(onresize_);
+
+resizeObserver.observe(viewer);
+
 let checkrs = true;
-function onresize_(e) {
+function onresize_() {
     if (checkrs) {
         let spanani =
             p.querySelector('span.alt') || p.querySelector('span.altshort');
---
+++ opentype.js
@@ -0,0 +1,5 @@
+const buffer = fetch('/AsAbove,SoBelow(Beta40)VF.ttf').then((res) =>
+    res.arrayBuffer()
+);
+
+let font;
