135f412bf804f7b99f07f758c4f5bc9e87b70df1
bao ann <baoanh1buinguyen@gmail.com>
2023-07-09 00:10:50 +0700

    someweirdshit

--- opentype.js
+++ opentype.js
@@ -1,5 +1,5 @@
-const buffer = fetch('/AsAbove,SoBelow(Beta40)VF.ttf').then((res) =>
-    res.arrayBuffer()
+const buffer = fetch('/AsAboveSoBelow/AsAbove,SoBelow(Beta40)VF.ttf').then(
+    (res) => res.arrayBuffer()
 );
 
 let font;
